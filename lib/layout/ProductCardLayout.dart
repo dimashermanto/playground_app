79f859b1805db4066cb07723d66a5458113445fb
dimashermanto <hermanto.dph@gmail.com>
2021-02-13 12:06:24 +0700

    Fixed Product card layout

---
+++ lib/layout/ProductCardLayout.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class ProductCard extends StatelessWidget {
+  const ProductCard({
+    Key key,
+    @required this.productList,
+    this.index
+  }) : super(key: key);
+
+  final List productList;
+  final int index;
+
+  @override
+  Widget build(BuildContext context) {
+
+    Radius cardBorderRadius = Radius.circular(12.0);
+
+    return Card(
+      elevation: 2,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(cardBorderRadius)),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 6,
+            ),
+            //Product Image
+            Expanded(
+                child: Center(
+                  child: Image.network(
+                      this.productList[index]["image"]),
+                )),
+            SizedBox(
+              height: 12,
+            ),
+
+            //Product Name
+            Text(this.productList[index]["title"], maxLines: 2,),
+            SizedBox(
+              height: 12,
+            ),
+
+            //Product Price
+            Text(
+              "\$${this.productList[index]["price"]}",
+              maxLines: 1,
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/layout/ProductListLayout.dart
+++ lib/layout/ProductListLayout.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:playground_app/layout/ProductCardLayout.dart';
 
 class ProductListLayout extends StatelessWidget {
   const ProductListLayout({
@@ -12,27 +13,14 @@ class ProductListLayout extends StatelessWidget {
   Widget build(BuildContext context) {
     return GridView.builder(
         gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-            maxCrossAxisExtent: 200,
-            crossAxisSpacing: 8,
-            mainAxisSpacing: 16),
+            maxCrossAxisExtent: 225,
+            mainAxisExtent: 225,
+            crossAxisSpacing: 6,
+            mainAxisSpacing: 8,
+        ),
         itemCount: this.productList.length,
         itemBuilder: (context, index) {
-          return Card(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                children: [
-                  Expanded(
-                      child: Image.network(
-                          this.productList[index]["image"])),
-                  SizedBox(
-                    height: 12,
-                  ),
-                  Text(this.productList[index]["title"], maxLines: 2,)
-                ],
-              ),
-            ),
-          );
+          return ProductCard(productList: productList, index: index);
         });
   }
 }
