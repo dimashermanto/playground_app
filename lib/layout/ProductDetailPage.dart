9d5c411e1bf1948e1881cc053ba5cd791fd603d8
dimashermanto <hermanto.dph@gmail.com>
2021-02-13 16:23:03 +0700

    Fixed ProductDetailPage.dart

--- lib/layout/ProductDetailPage.dart
+++ lib/layout/ProductDetailPage.dart
@@ -9,6 +9,7 @@ class _ProductDetailPageState extends State<ProductDetailPage> {
 
   Widget renderAppbar() {
     return AppBar(
+      title: Text("Product", style: TextStyle(color: Colors.black, fontSize: 16, fontWeight: FontWeight.w700, letterSpacing: 1.0),),
       iconTheme: IconThemeData(
         color: Colors.black, //change your color here
       ),
@@ -46,9 +47,9 @@ class _ProductDetailPageState extends State<ProductDetailPage> {
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text("Product 1 name", style: TextStyle(fontWeight: FontWeight.w700, fontSize: 20, letterSpacing: 1.0),),
+                      Text("Product 1 name", style: TextStyle(fontWeight: FontWeight.w700, fontSize: 16, letterSpacing: 1.0),),
                       SizedBox(height: 6,),
-                      Text("Product Category", style: TextStyle(color: Colors.grey[400], letterSpacing: 1.0),),
+                      Text("Product Category", style: TextStyle(color: Colors.grey[500], letterSpacing: 1.0, fontSize: 12),),
                     ],
                   ),
                 ),
@@ -57,7 +58,7 @@ class _ProductDetailPageState extends State<ProductDetailPage> {
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.end,
                     children: [
-                      Text("\$100", style: TextStyle(fontWeight: FontWeight.w700, fontSize: 20, letterSpacing: 1.0),),
+                      Text("\$100", style: TextStyle(fontWeight: FontWeight.w700, fontSize: 16, letterSpacing: 1.0),),
                     ],
                   ),
                 )
@@ -66,7 +67,7 @@ class _ProductDetailPageState extends State<ProductDetailPage> {
             SizedBox(height: 16,),
             Text(
                 "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
-                style: TextStyle(letterSpacing: 1.0, height: 1.6),
+                style: TextStyle(letterSpacing: 1.0, height: 1.6, ),
             )
           ],
         ),
